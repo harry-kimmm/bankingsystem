a8558e8b8e8ee82b1e4d31d4b75c6e14ff5c0691
Harry Kim <harryk9395@gmail.com>
2023-02-08 19:35:24 -0800

    Add base case for recursive method getBalanceOnYear

--- demo/src/main/java/com/example/InvestmentAccount.java
+++ demo/src/main/java/com/example/InvestmentAccount.java
@@ -1,25 +1,26 @@
 package com.example;
+
 import java.util.ArrayList;
 
 public class InvestmentAccount implements Account {
-    private double balance;
-    private String owner;
-    ArrayList<String> beneficiaries = new ArrayList<String>();
+    ArrayList<String> beneficiaries = new ArrayList<>();
+    double balance;
+    String owner;
 
     public InvestmentAccount(double bal, String own, ArrayList<String> ben) {
-        bal = balance;
-        own = owner;
-        ben = beneficiaries;
+        this.balance = bal;
+        this.owner = own;
+        this.beneficiaries = ben;
     }
 
-    public double deposit(int amt) {
-        this.balance += amt; 
+    public double deposit(double amt) {
+        this.balance += amt;
         return this.balance;
     }
 
-    public double withdraw(int amt) {
+    public double withdraw(double amt) {
         this.balance -= amt;
         return this.balance;
     }
 
-}   
+}
